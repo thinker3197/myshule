f51e1b9c93f50141dac419d30bda6862647f15dd
thinker3197 <ashishgupta.3197@gmail.com>
2018-01-14 14:41:21 +0530

    Add auth on post challenge page

--- src/containers/CreateChallengeForm.js
+++ src/containers/CreateChallengeForm.js
@@ -8,7 +8,9 @@ import SelectField from 'material-ui/SelectField'
 import MenuItem from 'material-ui/MenuItem'
 import Checkbox from 'material-ui/Checkbox'
 import RaisedButton from 'material-ui/RaisedButton'
-import Paper from 'material-ui/Paper'
+import Paper from 'material-ui/Paper';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 import LinearProgress from 'material-ui/LinearProgress';
 
 const items = [
@@ -68,13 +70,20 @@ class CreateChallengeForm extends Component {
         description: 'Compuatational thinking  has been defended by several authors as a thought process of solving problems compuatationally.' +
         'Many countries partcilaur in Europe and USA have been decrared integrating compuational thking in compusolry education.',
         category: 'Computational thinking',
-        file: null 
+        file: null,
+        user: null
     };
 
     // If challenge ref key was passed as props,
     // fetch the challenge and pre fill form data.
     // Set form in edit mode. 
     componentWillMount() {
+        const user = firebase.auth().currentUser;
+
+        this.setState({
+            user: user
+        });
+
         if(this.props.challangeRefKey) {
             firebase.database().ref('/challenges/' + this.props.challangeRefKey).on('value', (snapshot) => {
                 const data = snapshot.val();
@@ -201,79 +210,101 @@ class CreateChallengeForm extends Component {
         }
     }
 
+    onDialogClose = () => {
+        window.location.href = '/login';
+    }
+
     render() {
         const {redirect} = this.state;
+        const actions = [
+            <FlatButton
+              label="Close"
+              primary={true}
+              onClick={this.onDialogClose}
+            />,
+          ];
 
         if (redirect) {
             return <Redirect to='/challenges'/>;
         }
 
-        return <div style={{minHeight: '100vh', position: 'relative', 'paddingBottom': '8%'}}>
-            <NavBar title={this.state.editMode ? 'Edit challenge' : 'Post your challenge'}/>
-            <Paper style={{width: '50%', marginLeft: 'auto', marginRight: 'auto', padding: 20, marginTop: 50}}>
-                {
-                    this.state.loading &&
-                    <LinearProgress mode="indeterminate"/>
-                }
-                {/* <p style={{ fontFamily: 'Muli', fontSize: 25, fontWeight: '500'}}>Create a Challenge</p> */}
-                <TextField
+        if(this.state.user) {
+            return <div style={{minHeight: '100vh', position: 'relative', 'paddingBottom': '8%'}}>
+                <NavBar title={this.state.editMode ? 'Edit challenge' : 'Post your challenge'}/>
+                <Paper style={{width: '50%', marginLeft: 'auto', marginRight: 'auto', padding: 20, marginTop: 50}}>
+                    {
+                        this.state.loading &&
+                        <LinearProgress mode="indeterminate"/>
+                    }
+                    {/* <p style={{ fontFamily: 'Muli', fontSize: 25, fontWeight: '500'}}>Create a Challenge</p> */}
+                    <TextField
+                        style={{marginTop: 10, width: '60%'}}
+                        hintText="Name"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.handleTextChange}
+                    /><br/>
+                    <TextField
+                        style={{marginTop: 10, width: '60%'}}
+                        hintText="Email"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleTextChange}
+                    /><br/>
+                    <TextField
+                        style={{marginTop: 10, width: '60%'}}
+                        hintText="Title"
+                        name="title"
+                        value={this.state.title}
+                        onChange={this.handleTextChange}
+                    /><TextField
                     style={{marginTop: 10, width: '60%'}}
-                    hintText="Name"
-                    name="name"
-                    value={this.state.name}
+                    hintText="Description"
+                    name="description"
+                    multiLine={true}
+                    rows={5}
+                    value={this.state.description}
                     onChange={this.handleTextChange}
-                /><br/>
-                <TextField
-                    style={{marginTop: 10, width: '60%'}}
-                    hintText="Email"
-                    name="email"
-                    value={this.state.email}
-                    onChange={this.handleTextChange}
-                /><br/>
-                <TextField
-                    style={{marginTop: 10, width: '60%'}}
-                    hintText="Title"
-                    name="title"
-                    value={this.state.title}
-                    onChange={this.handleTextChange}
-                /><TextField
-                style={{marginTop: 10, width: '60%'}}
-                hintText="Description"
-                name="description"
-                multiLine={true}
-                rows={5}
-                value={this.state.description}
-                onChange={this.handleTextChange}
-            />
-                <br />
-                <br/>
-                <SelectField
-                    style={{marginTop: 10, width: '60%'}}
-                    value={this.state.category}
-                    onChange={this.handleCategoryChange.bind(this)}
-                    floatingLabelText="Pick Category"
-                >
-                    {items}
-                </SelectField>
-                <br/>
-                <RaisedButton
-                    style={{marginTop: 10, padding: 5}}
-                    containerElement='label'
-                    label='Upload File'>
-                    <input style={{marginLeft: 12}} type="file" onChange={this.handleFileSubmit.bind(this)}/>
-                </RaisedButton>
-                <br/>
-                <RaisedButton
-                    backgroundColor='#38c098'
-                    labelColor='#fff'
-                    style={{marginTop: 20}}
-                    buttonStyle={{background: 'linear-gradient(45deg, #00ead1 30%, #01ffb3 90%)'}}
-                    disabled={this.state.loading}
-                    label={this.state.editMode ? "Update" : "Submit"}
-                    onClick={this.state.editMode ? this.handleOnUpdate.bind(this) : this.handleOnSubmit.bind(this)}
                 />
-            </Paper>
-        </div>
+                    <br />
+                    <br/>
+                    <SelectField
+                        style={{marginTop: 10, width: '60%'}}
+                        value={this.state.category}
+                        onChange={this.handleCategoryChange.bind(this)}
+                        floatingLabelText="Pick Category"
+                    >
+                        {items}
+                    </SelectField>
+                    <br/>
+                    <RaisedButton
+                        style={{marginTop: 10, padding: 5}}
+                        containerElement='label'
+                        label='Upload File'>
+                        <input style={{marginLeft: 12}} type="file" onChange={this.handleFileSubmit.bind(this)}/>
+                    </RaisedButton>
+                    <br/>
+                    <RaisedButton
+                        backgroundColor='#38c098'
+                        labelColor='#fff'
+                        style={{marginTop: 20}}
+                        buttonStyle={{background: 'linear-gradient(45deg, #00ead1 30%, #01ffb3 90%)'}}
+                        disabled={this.state.loading}
+                        label={this.state.editMode ? "Update" : "Submit"}
+                        onClick={this.state.editMode ? this.handleOnUpdate.bind(this) : this.handleOnSubmit.bind(this)}
+                    />
+                </Paper>
+            </div>
+        } else {
+            return <Dialog
+                title="Cannot post challenge"
+                actions={actions}
+                modal={true}
+                open={!this.state.user}
+            >
+                You need to Login before you could post a challenge
+            </Dialog>
+        }
 
     }
 }
